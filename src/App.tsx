224415b8689f896392ba1106821d29cc1e911caf
yukun791745 <yukun791745@gmail.com>
2026-01-13 17:03:32 +0900

    Add route for AICoachPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AuthCallback from './pages/AuthCallback'
 import ActivityDetailPage from './pages/ActivityDetailPage'
+import AICoachPage from './pages/AICoachPage'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/activity/:id" element={<ActivityDetailPage />} />
+        <Route path="/ai" element={<AICoachPage />} />
       </Routes>
     </BrowserRouter>
   )
